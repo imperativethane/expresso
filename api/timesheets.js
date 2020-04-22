c7a58c4958eb5c1d23e6b481dcdb6f205c7cb29f
Derek Millar <derek.millartech@gmail.com>
2020-04-22 12:03:08 +0100

    added requests to edit and delete existing timesheets

--- api/timesheets.js
+++ api/timesheets.js
@@ -4,6 +4,22 @@ const timesheetsRouter = express.Router({mergeParams: true});
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+timesheetsRouter.param('timesheetId', (req, res, next, timesheetId) => {
+    const sql = 'SELECT * FROM Timesheet WHERE id = $timesheetId';
+    const values = {$timesheetId: timesheetId};
+
+    db.get(sql, values, (err, timesheet) => {
+        if (err) {
+            next(err);
+        } else if (!timesheet) {
+            res.sendStatus(404);
+        } else {
+            req.timesheet = timesheet;
+            next();
+        }
+    });
+});
+
 timesheetsRouter.get('/', (req, res, next) => {
     const sql = 'SELECT * FROM Timesheet WHERE employee_id = $employeeId';
     const values = {$employeeId: req.params.employeeId};
@@ -27,12 +43,12 @@ timesheetsRouter.post('/', (req, res, next) => {
     };
 
     const sql = 'INSERT INTO Timesheet (hours, rate, date, employee_id) ' +
-    'VALUES ($hours, $rate, $date, $employee_id)';
+    'VALUES ($hours, $rate, $date, $employeeId)';
     const values = {
         $hours: hours,
         $rate: rate,
         $date: date, 
-        $employee_id: req.params.employeeId
+        $employeeId: req.params.employeeId
     };
 
     db.run(sql, values, function(err) {
@@ -43,6 +59,50 @@ timesheetsRouter.post('/', (req, res, next) => {
             db.get(sql, (err, timesheet) => {
                 res.status(201).send({timesheet: timesheet});
             });
+        }
+    });
+});
+
+timesheetsRouter.put('/:timesheetId', (req, res, next) => {
+    const hours = req.body.timesheet.hours;
+    const rate = req.body.timesheet.rate;
+    const date = req.body.timesheet.date;
+
+    if (!hours || !rate || !date) {
+        res.sendStatus(400);
+    };
+
+    const sql = 'UPDATE Timesheet SET hours = $hours, rate = $rate, date = $date, employee_id = $employeeId ' +
+    'WHERE id = $timesheetId';
+    const values = {
+        $hours: hours,
+        $rate: rate,
+        $date: date, 
+        $employeeId: req.params.employeeId,
+        $timesheetId: req.params.timesheetId
+    };
+
+    db.run(sql, values, (err) => {
+        if (err) {
+            next(err);
+        } else {
+            const sql = `SELECT * FROM Timesheet WHERE id = ${req.params.timesheetId}`;
+            db.get(sql, (err, timesheet) => {
+                res.send({timesheet: timesheet});
+            });
+        }
+    });
+});
+
+timesheetsRouter.delete('/:timesheetId', (req, res, next) => {
+    const sql = 'DELETE FROM Timesheet WHERE id = $timesheetId';
+    const values = {$timesheetId: req.params.timesheetId};
+
+    db.run(sql, values, (err) => {
+        if (err) {
+            next(err);
+        } else {
+            res.sendStatus(204);
         }
     });
 });
